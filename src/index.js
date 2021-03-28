9b31cce761142dd79057ce28c3ebd3231e970204
nabilahaffryda <sehungantengs@gmail.com>
2021-03-28 23:33:36 +0700

    week 7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GenericContainer from "./GenericContainer/GenericContainerHOC";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GenericContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
